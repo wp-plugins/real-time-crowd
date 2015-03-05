87aab768745365dca1618a4769455b4a70012c4b
lgteamwork <lgteamwork@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-05 00:04:03 +0000

    updated to version 1.2 added admin real-time dashboard
    git-svn-id: https://plugins.svn.wordpress.org/real-time-crowd/tags/1.2@1105535 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Real-Time Crowd ===
+Contributors: realtimecrowd.net
+Tags: realtime, real time, real-time, realtime crowd, real time crowd, real-time crowd
+Requires at least: 3.6
+Tested up to: 4.1
+Stable tag: 1.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+The official RealTimeCrowd.net plugin for Wordpress.
+
+== Description ==
+
+This plugin enables Real-Time Crowd tracking and generation on your WordPress site.
+
+== Installation ==
+
+1. Upload the 'real-time-crowd' directory into the '/wp-content/plugins/' directory on your server.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Using the "WordPress *General* Settings" menu, navigate to Real-Time Crowd and enter your RealTimeCrowd.net Account alias.
+
+== Frequently Asked Questions ==
+
+== Changelog ==
+
+= 1.2 =
+* Added real-time dashboard to admin panel.
+
+= 1.1 =
+* Added checkbox for display/hide widget.
+
+= 1.0 =
+* First version.
+
+== Upgrade Notice ==
+
+= 1.2 =
+Easy upgrade
+
+= 1.1 =
+Easy upgrade
+
+= 1.0 =
+Required.
---
+++ real-time-crowd.php
@@ -0,0 +1,224 @@
+<?php
+/*
+Plugin Name: Real-Time Crowd
+Plugin URI: http://www.realtimecrowd.net/
+Description: Enables Real-Time Crowd tracking and generation on your WordPress site.
+Version: 1.2
+Author: RealTimeCrowd.net
+Author URI: http://www.realtimecrowd.net/
+License: GPL2
+*/
+function rtc_get_the_ID() 
+{
+	//$postid = url_to_postid($url);
+	if (in_the_loop())
+	{
+		$post_id = get_the_ID();
+	}
+	else
+	{
+		global $wp_query;
+		$post_id = $wp_query->get_queried_object_id();
+	}
+	return $post_id;
+}
+
+function rtc_install() 
+{
+	//Creates new database field
+	add_option("rtc_account_name", '', '', 'yes');
+	add_option("rtc_display_widget", '', '', 'yes');
+	add_option("rtc_widget_title", 'Being read right now', '', 'yes');
+}
+function rtc_uninstall() 
+{
+	//Deletes the database field
+	//delete_option('rtc_account_name');
+	//delete_option('rtc_display_widget');
+	//delete_option('rtc_widget_title');
+}
+
+function rtc_admin_action_links( $links ) 
+{
+   // add Settings link
+   $links[] = '<a href="'. get_admin_url(null, 'admin.php?page=real-time-crowd') .'">Settings</a>';
+   //$links[] = '<a href="http://www.realtimecrowd.net" target="_blank">More info</a>';
+   return $links;
+}
+
+function rtc_admin_left_menu() 
+{
+	add_menu_page('Real-Time Crowd plugin', 'Real-Time Crowd', 'manage_options', 'real-time-crowd', 'rtc_admin_html_page');
+}
+
+function rtc_admin_html_page() 
+{
+?>
+<div class="wrap">
+  <div id="icon-plugins" class="icon32"></div>
+  <h2>Real-Time Crowd</h2>
+  <form method="POST" action="options.php">
+    <?php wp_nonce_field('update-options'); ?>
+	<?php if( isset($_GET['settings-updated']) ) { ?>
+    <div id="message" class="updated fade">
+        <p><strong><?php _e('Settings saved.') ?></strong></p>
+    </div>
+<?php } ?>
+	<p style="width: 80%;">
+		First you need to register with <b><a target="_blank" href="http://www.realtimecrowd.net/">RealTimeCrowd.net</a></b>
+		<br/>
+		Then please input your RealTimeCrowd <b>account alias</b>, and check <b>Display Widget</b> box below.
+		<br/>
+		Visit <b><a target="_blank" href="http://www.realtimecrowd.net/Partner/Account">http://www.realtimecrowd.net/Partner/Account</a></b> to see a list of all your accounts.
+	</p>
+    <table class="form-table">
+      <tr valign="top">
+        <th scope="row">
+          <label for="rtc_account_name">Account alias</label>
+        </th>
+        <td>
+          <input name="rtc_account_name" value="<?php echo get_option('rtc_account_name'); ?>" class="regular-text" />
+          <span class="description">(ex: superblogger)</span>
+        </td>
+      </tr>
+	  <tr valign="top">
+        <th scope="row">
+          <label for="rtc_display_widget">Display Widget</label>
+        </th>
+        <td>
+          <input name="rtc_display_widget" type="checkbox" value="1" <?php checked( '1', get_option( 'rtc_display_widget' ) ); ?> />
+          <span class="description">Checking this will display the widget on every post and page of the site. (The widget also contains a small icon linking to RealTimeCrowd.net)</span>
+        </td>
+      </tr>
+	  <tr valign="top">
+        <th scope="row">
+          <label for="rtc_widget_title">Widget title</label>
+        </th>
+        <td>
+          <input name="rtc_widget_title" value="<?php echo get_option('rtc_widget_title'); ?>" class="regular-text" />
+          <span class="description">(ex: Being read right now)</span>
+        </td>
+      </tr>
+    </table>   
+    <input type="hidden" name="action" value="update" />
+    <input type="hidden" name="page_options" value="rtc_account_name,rtc_display_widget,rtc_widget_title" />
+    <p class="submit">
+      <input class="button-primary" type="submit" name="Save" value="<?php _e('Save'); ?>" />
+    </p>
+	<p style="width: 80%;">
+		<br/>
+		Send us <a target="_blank" href="http://www.realtimecrowd.net/Contact/">Feedback</a>
+	</p>
+  </form>
+	<?php 
+		$accountAlias = get_option("rtc_account_name");
+		if (!empty($accountAlias))
+		{
+		?>
+			<div>
+				<h4>Real-Time data for account: <?php echo $accountAlias ?></h4>
+				<iframe src="http://rtc.realtimecrowd.net/viewer/v1-1/<?php echo $accountAlias ?>/" style="border:none;width:100%;height:580px;"></iframe>
+			</div>
+		<?php
+		}
+		else
+		{
+		?>
+			<div>
+				<h4>Save your Account Alias to display Real-Time data from your website</h4>
+			</div>
+		<?php
+		}
+		?>
+  	
+</div>
+<?php
+}
+function rtc_tracking_code() 
+{
+	$accountAlias = get_option("rtc_account_name");
+	$widgetTitle = get_option("rtc_widget_title");
+	$displayWidget = get_option("rtc_display_widget");
+	$pageTitle = "";
+	$pageImageUrl = "";
+	$postId = rtc_get_the_ID();
+	if (empty($postId))
+	{
+		$postId = 0;
+	}
+	
+	if (empty($widgetTitle)) 
+	{
+		$widgetTitle = "Being read right now";
+	}
+	if ($displayWidget == "1")
+	{
+		$displayWidget = "true";
+	}
+	else	
+	{
+		$displayWidget = "false";
+	}
+
+	if ($postId > 0)
+	{
+		$pageTitle = get_the_title($postId);
+		if (has_post_thumbnail($postId))
+		{
+			$image = wp_get_attachment_image_src(get_post_thumbnail_id($postId), array(100, 100));
+			if (count($image)>0)
+			{
+				$pageImageUrl = $image[0];
+			}
+		}
+	}
+	else if (is_front_page())
+	{
+		$pageTitle = get_bloginfo('name');
+	}
+
+	if (!empty($accountAlias)) 
+	{
+		echo "<!--rtc code-->
+		<div id='rtc-div-main'>
+		</div>
+		<script>
+			!function (rtcObj, topElem)
+			{
+				function embedRtcLauncher(oUrl)
+				{
+					var scriptElem = topElem.createElement('script');
+					scriptElem.type = 'text/javascript';
+					scriptElem.async = !0;
+					scriptElem.src = ('https:' == topElem.location.protocol ? 'https' : 'http') + ':' + oUrl + '/Scripts/RteVisitorLauncher-2.0.js';
+					topElem.body.appendChild(scriptElem);
+				}
+				rtcObj.Account = '".$accountAlias."';
+				rtcObj.HeaderText = '".$widgetTitle."';
+				rtcObj.Color = '#555';
+				rtcObj.BorderColor = '#444444';
+				rtcObj.Position = 'right';
+				rtcObj.ContainerId = 'rtc-div-main';
+				rtcObj.RtcUrl = '//rtc.realtimecrowd.net';
+				rtcObj.HideWidget = !".$displayWidget.";
+				rtcObj.PageTitle = '".$pageTitle."';
+				rtcObj.PageImageUrl = '".$pageImageUrl."';
+				embedRtcLauncher(rtcObj.RtcUrl);
+			}
+			(window.RTC = {}, document);
+		</script>
+		<!--/rtc code-->";
+	}
+}
+/* Puts code on Wordpress pages */
+add_action('wp_footer', 'rtc_tracking_code');
+
+/* Runs when plugin is activated */
+register_activation_hook(__FILE__, 'rtc_install');
+
+/* Runs on plugin deactivation*/
+register_deactivation_hook(__FILE__, 'rtc_uninstall' );
+
+add_action('admin_menu', 'rtc_admin_left_menu');
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'rtc_admin_action_links' ); 
+?>
